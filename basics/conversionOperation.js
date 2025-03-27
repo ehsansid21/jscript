2b57f4dcafa5df5476871096a5344918ccb81cae
ehsansid21 <ehsansid6357@gmail.com>
2025-03-27 18:40:08 +0000

    conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33"
+
+//console.log(typeof score);
+
+//let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn ="ehsan"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=>false
+// "" => false
+// "ehsan" => true
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+let accountId=144553
+let accountCity= "Bengaluru"
+//accountId=4234
+
+console.log(accountCity)
+console.table([accountId, accountCity])
+/*
+prefer not use var 
+because of issue in 
+block scope and function scope
+*/
