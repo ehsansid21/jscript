afec47890f45b54c5eb1b6e146c39ad5c9cdab30
ehsansid21 <ehsansid6357@gmail.com>
2025-03-27 18:16:53 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all js code as newer version
+// alert( 3 + 3) // we are using node.js,not browser
+
+let name="ehsan"
+let age=21
+let isLoggedIn= false
+// number=>2 to power 53
+// bigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+
+//object
+console.log(typeof name)
+console.log(typeof undefined)
+console.log(typeof null)
