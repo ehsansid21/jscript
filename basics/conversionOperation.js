eeff69ca40be67dfa46475cacaecde21ea849c0c
ehsansid21 <ehsansid6357@gmail.com>
2025-03-27 19:19:07 +0000

    operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -10,10 +10,47 @@ let score="33"
 // "33abc" => NaN
 // true => 1; false => 0;
 
-let isLoggedIn ="ehsan"
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn ="ehsan"
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=>false
 // "" => false
 // "ehsan" => true
+
+//********************** Operations ***********************
+
+let value = 3
+let negVal= -value
+//console.log(negVal);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 ="hello"
+let str2 =" ehsan"
+
+let str3= str1+ str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
